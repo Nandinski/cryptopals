c908f26b509fb659375e3b34789d9eb81a709ee2
Nandinski <nando_14sclm@hotmail.com>
2018-07-08 19:39:58 +0100

    Styling done by golint

--- lib/cryptolab.go
+++ lib/cryptolab.go
@@ -1,16 +1,15 @@
-// Cryptopals
 package lib
 
 import (
-	"Cryptopals/lib/enc/hex"
 	"Cryptopals/lib/enc/base64"
+	"Cryptopals/lib/enc/hex"
 	"Cryptopals/lib/enc/xor"
-	"fmt"
-	"errors"
-	"strings"
 	"bufio"
+	"errors"
+	"fmt"
 	"os"
 	"sort"
+	"strings"
 )
 
 // *************************
@@ -19,8 +18,7 @@ import (
 //
 // *************************
 
-//
-// Convert hex to base64
+// ConvertHexTo64 converts hex string to base64
 func ConvertHexTo64(hexString string) string {
 	fmt.Printf("\nHex\n%s", hexString)
 	fmt.Println()
@@ -29,16 +27,13 @@ func ConvertHexTo64(hexString string) string {
 	checkError(err)
 	fmt.Printf("\nDecodedHex\n%s", decodedHexString)
 
-
 	encodedbase64String := base64.EncodeToString(decodedHexString)
 	fmt.Printf("\nBase64\n%s", encodedbase64String)
 
 	return encodedbase64String
 }
 
-//
-// Fixed XOR
-
+// FixedXOR does the xor between two strings
 func FixedXOR(s1, s2 string) string {
 	fmt.Printf("\nHex1\n%s\n", s1)
 	fmt.Print("XOR")
@@ -57,24 +52,22 @@ func FixedXOR(s1, s2 string) string {
 	return hexEncodedXOR
 }
 
-var TryingToXORDifSizedValues = errors.New("Can't perform XOR on different sized arrays")
-
-func XOROverByteVector(b1, b2 []byte) ([]byte, error){
+// XOROverByteVector performs XOR over 2 byte arrays of the same length
+func XOROverByteVector(b1, b2 []byte) ([]byte, error) {
 	if len(b1) != len(b2) {
-		return b1, TryingToXORDifSizedValues
+		return b1, errors.New("Can't perform XOR on different sized arrays")
+
 	}
 	xored := make([]byte, len(b1))
 
-	for i := range(b1) {
-		xored[i] = b1[i]^b2[i]
+	for i := range b1 {
+		xored[i] = b1[i] ^ b2[i]
 	}
 
 	return xored, nil
 }
 
-//
-// Single-byte XOR cipher
-
+// SingleKeyXORDecipher Breaks single byte XOR on hexString
 func SingleKeyXORDecipher(hexString string) (byte, string) {
 	// Convert hex to bytes
 	decodedHexString, err := hex.DecodeString(hexString)
@@ -85,6 +78,7 @@ func SingleKeyXORDecipher(hexString string) (byte, string) {
 	return key, plaintext
 }
 
+// BreakSingleByteXOR -
 func BreakSingleByteXOR(b []byte) (string, byte, float64) {
 	var key byte
 	var plainTextMaxScore string
@@ -107,6 +101,7 @@ func BreakSingleByteXOR(b []byte) (string, byte, float64) {
 }
 
 func xorByByte(key byte, b []byte) string {
+
 	decipheredText := make([]byte, len(b))
 
 	for i := range b {
@@ -116,54 +111,51 @@ func xorByByte(key byte, b []byte) string {
 	return string(decipheredText)
 }
 
-var EnglishFrequencyTable = map[rune]float64 {
-	' ' : 0.1918182,	// This was the key to break it!
-	'a' : 0.0651738,
-	'b' : 0.0124248,
-	'c' : 0.0217339,
-	'd' : 0.0349835,
-	'e' : 0.1041442,
-	'f' : 0.0197881,
-	'g' : 0.0158610,
-	'h' : 0.0492888,
-	'i' : 0.0558094,
-	'j' : 0.0009033,
-	'k' : 0.0050529,
-	'l' : 0.0331490,
-	'm' : 0.0202124,
-	'n' : 0.0564513,
-	'o' : 0.0596302,
-	'p' : 0.0137645,
-	'q' : 0.0008606,
-	'r' : 0.0497563,
-	's' : 0.0515760,
-	't' : 0.0729357,
-	'u' : 0.0225134,
-	'v' : 0.0082903,
-	'w' : 0.0171272,
-	'x' : 0.0013692,
-	'y' : 0.0145984,
-	'z' : 0.0007836,
+// EnglishFrequencyTable contains the frequency of each letter commonly used in english
+var EnglishFrequencyTable = map[rune]float64{
+	' ': 0.1918182, // This was the key to break it!
+	'a': 0.0651738,
+	'b': 0.0124248,
+	'c': 0.0217339,
+	'd': 0.0349835,
+	'e': 0.1041442,
+	'f': 0.0197881,
+	'g': 0.0158610,
+	'h': 0.0492888,
+	'i': 0.0558094,
+	'j': 0.0009033,
+	'k': 0.0050529,
+	'l': 0.0331490,
+	'm': 0.0202124,
+	'n': 0.0564513,
+	'o': 0.0596302,
+	'p': 0.0137645,
+	'q': 0.0008606,
+	'r': 0.0497563,
+	's': 0.0515760,
+	't': 0.0729357,
+	'u': 0.0225134,
+	'v': 0.0082903,
+	'w': 0.0171272,
+	'x': 0.0013692,
+	'y': 0.0145984,
+	'z': 0.0007836,
 }
-
 
 func computeScore(text string) float64 {
 	var score float64
-	for _, char := range(strings.ToLower(text)) {
+	for _, char := range strings.ToLower(text) {
 		if char > 126 {
 			return 0
 		}
-		if val, ok := EnglishFrequencyTable[char]; ok{
+		if val, ok := EnglishFrequencyTable[char]; ok {
 			score += val
 		}
 	}
 	return score
 }
 
-
-//
-// Detect single-character XOR
-
+// DetectSingleCharacterXORInFile -
 func DetectSingleCharacterXORInFile(filename string) (byte, string) {
 	file, err := os.Open(filename)
 	checkError(err)
@@ -193,8 +185,7 @@ func DetectSingleCharacterXORInFile(filename string) (byte, string) {
 	return key, plainTextMaxScore
 }
 
-//
-// Implement repeating-key XOR
+// EncodeWithRepeatingXOR Implement repeating-key XOR
 func EncodeWithRepeatingXOR(key string, plainText string) (string, error) {
 	cypheredText, err := xor.EncodeWithRepeatingXOR(key, plainText)
 	checkError(err)
@@ -206,6 +197,7 @@ func EncodeWithRepeatingXOR(key string, plainText string) (string, error) {
 //
 // Break repeating-key XOR
 
+// HammingDistance -
 // Calculate the ammount of diferent bits
 func HammingDistance(a, b []byte) int {
 	if len(a) != len(b) {
@@ -233,26 +225,26 @@ func HammingDistance(a, b []byte) int {
 
 type scoreKey struct {
 	score float64
-	key int
+	key   int
 }
 
 func findRepeatingXORSize(cypheredText []byte) []int {
 	var res []scoreKey
 	for keySize := 2; keySize < 40; keySize++ {
 		// for each 'keySize' size of text do
-		averageHD :=  0.
+		averageHD := 0.
 
 		blockCount := len(cypheredText) / keySize
-		for lBlockIndex := 0; lBlockIndex < blockCount - 1; lBlockIndex++ {
+		for lBlockIndex := 0; lBlockIndex < blockCount-1; lBlockIndex++ {
 			normalizedHD := 0.
-			leftBlock := cypheredText[lBlockIndex * keySize : (lBlockIndex + 1) * keySize]
+			leftBlock := cypheredText[lBlockIndex*keySize : (lBlockIndex+1)*keySize]
 
 			for rBlockIndex := lBlockIndex + 1; rBlockIndex < blockCount; rBlockIndex++ {
-				rightBlock := cypheredText[rBlockIndex * keySize : (rBlockIndex + 1) * keySize]
+				rightBlock := cypheredText[rBlockIndex*keySize : (rBlockIndex+1)*keySize]
 
 				normalizedHD += float64(HammingDistance(leftBlock, rightBlock)) / float64(keySize)
 			}
-			averageHD = normalizedHD / float64(blockCount - lBlockIndex)
+			averageHD = normalizedHD / float64(blockCount-lBlockIndex)
 		}
 		// a, b := cypheredText[:keySize*4], cypheredText[keySize*4 : keySize*4 * 2]
 		// averageHD = float64(HammingDistance(a, b)) / float64(keySize)
@@ -266,20 +258,25 @@ func findRepeatingXORSize(cypheredText []byte) []int {
 	return bestKeys[:3]
 }
 
-func BreakRepeatingXORCypher(cypheredText []byte) ([]byte, []byte){
+// BreakRepeatingXORCypher -
+// First guesses the top 3 lengths of the key
+// Breakes Single Byte XOR for each byte cyphered with the same key
+func BreakRepeatingXORCypher(cypheredText []byte) ([]byte, []byte) {
 	// Try to find the key length
 	likelyKeySize := findRepeatingXORSize(cypheredText)
 
 	var bestScore float64
 	var bestKey []byte
-	for _, keySize  := range likelyKeySize {
+	for _, keySize := range likelyKeySize {
 		column := make([]byte, len(cypheredText)/keySize)
 		cypherKey := make([]byte, keySize)
 
 		keyScore := 0.
 		for col := 0; col < keySize; col++ {
-			for row := 0; row < len(column) - 1; row++ {
-				if row * keySize + col >= len(cypheredText) { continue } // Ignore it if you can't read it
+			for row := 0; row < len(column)-1; row++ {
+				if row*keySize+col >= len(cypheredText) {
+					continue
+				} // Ignore it if you can't read it
 				column[row] = cypheredText[row*keySize+col]
 			}
 			_, key, score := BreakSingleByteXOR(column)
--- lib/util.go
+++ lib/util.go
@@ -1,11 +1,11 @@
-
 package lib
 
 import (
-	"io/ioutil"
 	"Cryptopals/lib/enc/base64"
+	"io/ioutil"
 )
 
+// ReadBase64FromFile reads bytes from file in base 64
 func ReadBase64FromFile(filename string) ([]byte, error) {
 	dataB64, err := ioutil.ReadFile(filename)
 	if err != nil {
